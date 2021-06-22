e01df9658f5218fb01b59c8e602e862bb54e2866
olivejua <tmfrl4710@gmail.com>
2021-06-22 18:36:56 +0900

    Add Files: UserController 인터페이스, UserController 구현체

---
+++ src/main/java/com/moneelab/assignment/web/controller/user/UserController.java
@@ -0,0 +1,9 @@
+package com.moneelab.assignment.web.controller.user;
+
+import com.moneelab.assignment.web.controller.Controller;
+
+public interface UserController extends Controller {
+
+    String signin();
+    String signup();
+}
---
+++ src/main/java/com/moneelab/assignment/web/controller/user/UserControllerImpl.java
@@ -0,0 +1,14 @@
+package com.moneelab.assignment.web.controller.user;
+
+public class UserControllerImpl implements UserController {
+
+    public String signup() {
+        // do process
+        return "result signup";
+    }
+
+    public String signin() {
+        // do process
+        return "result signin";
+    }
+}
