8cd7f68ce6896a076b4cbb40b8713ff62fe936cd
olivejua <tmfrl4710@gmail.com>
2021-06-22 18:37:24 +0900

    Add Files: PostController 인터페이스, PostController 구현체

---
+++ src/main/java/com/moneelab/assignment/web/controller/post/PostController.java
@@ -0,0 +1,10 @@
+package com.moneelab.assignment.web.controller.post;
+
+import com.moneelab.assignment.web.controller.Controller;
+
+public interface PostController extends Controller {
+
+    String save();
+    String update();
+    String delete();
+}
---
+++ src/main/java/com/moneelab/assignment/web/controller/post/PostControllerImpl.java
@@ -0,0 +1,19 @@
+package com.moneelab.assignment.web.controller.post;
+
+public class PostControllerImpl implements PostController {
+
+    @Override
+    public String save() {
+        return "result save";
+    }
+
+    @Override
+    public String update() {
+        return "result update";
+    }
+
+    @Override
+    public String delete() {
+        return "result delete";
+    }
+}
