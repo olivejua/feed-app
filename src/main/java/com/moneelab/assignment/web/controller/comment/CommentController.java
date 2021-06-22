b78d2f55377dd4e5bf33f4f3e439b8c945e5f627
olivejua <tmfrl4710@gmail.com>
2021-06-22 18:38:02 +0900

    Add Files: CommentController 인터페이스, CommentController 구현체

---
+++ src/main/java/com/moneelab/assignment/web/controller/comment/CommentController.java
@@ -0,0 +1,9 @@
+package com.moneelab.assignment.web.controller.comment;
+
+import com.moneelab.assignment.web.controller.Controller;
+
+public interface CommentController extends Controller {
+    String save();
+    String update();
+    String delete();
+}
---
+++ src/main/java/com/moneelab/assignment/web/controller/comment/CommentControllerImpl.java
@@ -0,0 +1,18 @@
+package com.moneelab.assignment.web.controller.comment;
+
+public class CommentControllerImpl implements CommentController {
+    @Override
+    public String save() {
+        return "result save";
+    }
+
+    @Override
+    public String update() {
+        return "result update";
+    }
+
+    @Override
+    public String delete() {
+        return "result delete";
+    }
+}
