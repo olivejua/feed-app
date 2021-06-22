afbd571db369911007fa5c75c584a3622c2d6e9c
olivejua <tmfrl4710@gmail.com>
2021-06-22 18:25:35 +0900

    add Files: util/HttpMethods, util/PathConstants
    util/HttpMethods -> HTTP Method 상수 목록
    util/PathConstants -> Request URI 경로 상수 목록

---
+++ src/main/java/com/moneelab/assignment/util/HttpMethods.java
@@ -0,0 +1,9 @@
+package com.moneelab.assignment.util;
+
+public abstract class HttpMethods {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+    public static final String PATCH = "PATCH";
+}
---
+++ src/main/java/com/moneelab/assignment/util/PathConstants.java
@@ -0,0 +1,28 @@
+package com.moneelab.assignment.util;
+
+public abstract class PathConstants {
+    //Common Path for All
+    public static final String COMMON_ALL = "/api/v1";
+
+    //User Path
+    public static final String USER = COMMON_ALL + "/user";
+    public static final String USER_SIGN_UP = USER + "/signup";
+    public static final String USER_SIGN_IN = USER + "/signin";
+
+    //Feed Path
+    public static final String FEED = COMMON_ALL  + "/feed";
+
+    //Common CUD Path
+    public static final String SAVE = "/save";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete";
+
+    //Post Path
+    public static final String COMMON_POST = COMMON_ALL + "/post";
+
+    //Comment Path
+    public static final String COMMON_COMMENT = COMMON_ALL + "/comment";
+
+    public static final String COMMON_LIKE = COMMON_ALL + "/like";
+
+}
