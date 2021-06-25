7114e1e926a10bbe91cfa258e0b20bdc83aeebc2
olivejua <tmfrl4710@gmail.com>
2021-06-26 01:08:40 +0900

    Refactoring: 상수명 변경

--- src/main/java/com/moneelab/assignment/util/PathConstants.java
+++ src/main/java/com/moneelab/assignment/util/PathConstants.java
@@ -2,24 +2,24 @@ package com.moneelab.assignment.util;
 
 public abstract class PathConstants {
     //Common Path for All
-    public static final String COMMON_ALL = "/api/v1";
+    public static final String P_COMMON = "/api/v1";
 
     //User Path
-    public static final String USER = COMMON_ALL + "/user";
-    public static final String ALL_USERNAMES = USER + "/allUsernames";
-    public static final String USER_SIGN_UP = USER + "/signUp";
-    public static final String USER_SIGN_IN = USER + "/signIn";
-    public static final String USER_LOGOUT = USER + "/logout";
+    public static final String P_USER = P_COMMON + "/user";
+    public static final String P_ALL_USERNAMES = P_USER + "/allUsernames";
+    public static final String P_SIGN_UP = P_USER + "/signUp";
+    public static final String P_SIGN_IN = P_USER + "/signIn";
+    public static final String P_LOGOUT = P_USER + "/logout";
 
     //Feed Path
-    public static final String FEED = COMMON_ALL  + "/feed";
+    public static final String P_FEED = P_COMMON + "/feed";
 
     //Post Path
-    public static final String COMMON_POST = COMMON_ALL + "/post";
+    public static final String P_POST = P_COMMON + "/post";
 
     //Comment Path
-    public static final String COMMON_COMMENT = COMMON_ALL + "/comment";
+    public static final String P_COMMENT = P_COMMON + "/comment";
 
-    public static final String COMMON_LIKE = COMMON_ALL + "/like";
-
+    //Like Path
+    public static final String P_LIKE = P_COMMON + "/like";
 }
